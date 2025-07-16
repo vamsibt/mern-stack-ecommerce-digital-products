6a8c9b1f27ca9708d193265b0884f55313a0ad33
vamsibt <134447161+vamsibt@users.noreply.github.com>
2025-07-16 16:36:29 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,66 +1,118 @@
-
-import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import React, { useContext, useEffect, useState } from "react";
+import { ShopContext } from "../context/ShopContext";
 import axios from "axios";
+import { toast } from "react-toastify";
 
 const Login = () => {
-    const navigate = useNavigate();
+  const [currentState, setCurrentState] = useState("Login");
+  const { token, setToken, navigate, backendUrl } = useContext(ShopContext);
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
 
-    const [loginData, setLoginData] = useState({
-        email: '',
-        password: ''
-    });
-
-    const [statusMessage, setStatusMessage] = useState('');
-
-    const changeInputHandler = (e) => {
-        setLoginData({ ...loginData, [e.target.name]: e.target.value });
-        if (statusMessage) setStatusMessage('');
-    };
-
-    const submitHandler = async (e) => {
-        e.preventDefault();
-        try {
-            const response = await axios.post(`${import.meta.env.VITE_REACT_APP_BACKEND_BASEURL}/auth/login`, loginData);
-            console.log(response.data);
-
-            localStorage.setItem('token', response.data.token);
-
-            navigate('/admin');
-        } catch (error) {
-            if (error.response) {
-              setStatusMessage(error.response.data.msg);
-            } else if (error.request) {
-                console.log(error.request);
-            } else {
-                console.log('Error', error.message);
-            }
+  const onSubmitHandler = async (event) => {
+    event.preventDefault();
+    try {
+      if (currentState == "Sign Up") {
+        console.log("🚀 SignUp Data:", { name, email, password }); 
+        const response = await axios.post(backendUrl + "/api/user/register", {
+          name,
+          email,
+          password,
+        });
+        if (response.data.success) {
+          setToken(response.data.token);
+          localStorage.setItem("token", response.data.token);
+        } else {
+          toast.error(response.data.message);
         }
-    };
+      } else {
+        const response = await axios.post(backendUrl + "/api/user/login", {
+          email,
+          password,
+        });
+        if (response.data.success) {
+          setToken(response.data.token);
+          localStorage.setItem("token", response.data.token);
+        } else {
+          toast.error(response.data.message);
+        }
+      }
+    } catch (error) {}
+  };
 
+  useEffect(() => {
+    if (token) {
+      navigate("/");
+    }
+  });
 
   return (
-    <div className='flex flex-col items-center justify-center min-h-screen bg-base-100'>
-        <h2 className='text-2xl font-bold mb-10'>Log In</h2>
-        {statusMessage && <p className='text-red-500 text-xs italic mb-2'>{statusMessage}</p>}
+    <form
+      onSubmit={onSubmitHandler}
+      className="flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800"
+    >
+      <div className="inline-flex items-center gap-2 mt-10 mb-2">
+        <p className="text-3xl prata-regular dark:text-white">{currentState}</p>
+        <hr className="border-none h-[1.5px] w-8 bg-gray-800 dark:bg-white" />
+      </div>
+      {currentState === "Login" ? (
+        ""
+      ) : (
+        <input
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+          type="text"
+          className="w-full px-3 py-2 border border-gray-800 "
+          placeholder="Name"
+          required
+        />
+      )}
+      <input
+        onChange={(e) => setEmail(e.target.value)}
+        value={email}
+        type="email"
+        className="w-full px-3 py-2 border border-gray-800 dark:border-gray-300"
+        placeholder="Email"
+        required
+      />
+      <input
+        onChange={(e) => setPassword(e.target.value)}
+        value={password}
+        type="password"
+        className="w-full px-3 py-2 border border-gray-800 dark:border-gray-300"
+        placeholder="Password"
+        required
+      />
+      {currentState === "Login" ? (
+        <div className="flex w-full justify-between text-sm mt-[-8px]">
+          <p className="cursor-pointer active:text-blue-500 dark:text-gray-200">
+            Forget password?
+          </p>
+          <p
+            onClick={() => setCurrentState("Sign Up")}
+            className="cursor-pointer active:text-blue-500 dark:text-gray-200"
+          >
+            Create account
+          </p>
+        </div>
+      ) : (
+        <div className="flex w-full justify-between text-sm mt-[-8px]">
+          <p></p>
+          <p
+            onClick={() => setCurrentState("Login")}
+            className="cursor-pointer active:text-blue-500 dark:text-gray-200"
+          >
+            Have an Account? Login Here
+          </p>
+        </div>
+      )}
 
-        <form className='w-full max-w-xs' onSubmit={submitHandler}>
-            <input type="email" placeholder='Email' name="email" value={loginData.email} onChange={changeInputHandler}
-            className='shadow border rounded-md w-full py-2 px-3 text-gray-700'
-            />
+      <button className="px-8 py-2 mt-4 font-light text-white bg-black active:bg-gray-700 dark:text-black dark:bg-white">
+        {currentState === "Login" ? "Sign In" : "Sign Up"}
+      </button>
+    </form>
+  );
+};
 
-            <input type="password" placeholder='Password' name="password" value={loginData.password} onChange={changeInputHandler}
-            className='shadow border rounded-md w-full py-2 px-3 text-gray-700'
-            />
-
-            <button type='submit' className='bg-blue-500 hover:bg-blue-800 rounded-md py-2 px-4 w-full'>Log In</button>
-
-        </form>
-
-        <p className='mt-4'>No account yet?</p>
-        <Link to="/register" className='text-blue-500 hover:text-blue-800 text-xl'>Register</Link>
-    </div>
-  )
-}
-
-export default Login
+export default Login;
